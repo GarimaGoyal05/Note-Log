3f13614e0b5e295c49564be83c2bd757ae5ccb52
Garima <123671271+GarimaKhanijo@users.noreply.github.com>
2023-05-21 15:50:17 +0530

    additional changes updated.

--- test.js
+++ test.js
@@ -19,6 +19,30 @@ function displayDate(){
   document.querySelector("#date").innerHTML = date; 
 }
 
+function displayItems(){
+  let items = "";
+  for(let i = 0; i < itemsArray.length; i++){
+    items += `<div class="item">
+                <div class="input-controller">
+                  <textarea disabled>${itemsArray[i]}</textarea>
+                  <div class="edit-controller">
+                    <i class="fa-solid fa-check deleteBtn"></i>
+                    <i class="fa-solid fa-pen-to-square editBtn"></i>
+                  </div>
+                </div>
+                <div class="update-controller">
+                  <button class="saveBtn">Save</button>
+                  <button class="cancelBtn">Cancel</button>
+                </div>
+              </div>`
+  }
+  document.querySelector(".to-do-list").innerHTML = items;
+  activateDeleteListeners();
+  activateEditListeners();
+  activateSaveListeners();
+  activateCancelListeners();
+}
+
 function activateDeleteListeners(){
   let deleteBtn = document.querySelectorAll(".deleteBtn");
   deleteBtn.forEach((db, i) => {
@@ -37,6 +61,29 @@ function activateEditListeners(){
   })
 }
 
+function activateSaveListeners(){
+  const saveBtn = document.querySelectorAll(".saveBtn");
+  const inputs = document.querySelectorAll(".input-controller textarea");
+  saveBtn.forEach((sb, i) => {
+    sb.addEventListener("click", () => {
+      updateItem(inputs[i].value, i);
+    })
+  })
+}
+
+function activateCancelListeners(){
+  const cancelBtn = document.querySelectorAll(".cancelBtn");
+  const updateController = document.querySelectorAll(".update-controller");
+  const inputs = document.querySelectorAll(".input-controller textarea");
+  cancelBtn.forEach((cb, i) => {
+    cb.addEventListener("click", () => {
+      updateController[i].style.display = "none";
+      inputs[i].disabled = true; 
+      inputs[i].style.border = "none";
+    })
+  })
+}
+
 function createItem(item){
   itemsArray.push(item.value);
   localStorage.setItem('items', JSON.stringify(itemsArray));
@@ -47,4 +94,15 @@ function deleteItem(i){
   itemsArray.splice(i,1);
   localStorage.setItem('items', JSON.stringify(itemsArray)); 
   location.reload();
+}
+
+function updateItem(text, i){
+  itemsArray[i] = text;
+  localStorage.setItem('items', JSON.stringify(itemsArray));
+  location.reload();
+}
+
+window.onload = function() {
+  displayDate();
+  displayItems();
 }
